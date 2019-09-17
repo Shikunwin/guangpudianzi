880f74dab57885d843b3a74a2b44ef219a0c2323
Shikunwin <2284692254@qq.com>
2019-09-17 08:25:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# guangpudianzi
---
+++ js/lihe.js
@@ -0,0 +1,10 @@
+$(function(){
+	var swiper=new Swiper('.swiper-container',{
+        pagination:'.swiper-pagination',
+        paginationClickable:true,
+		spaceBetween:30,
+		loop:true,
+		autoplay:3000
+    });
+
+})
